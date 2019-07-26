00344be8e5dde127701d38ad5b42da6f282c16f2
Phan Thanh Nam <40748333+phannam97@users.noreply.github.com>
2019-07-26 22:47:15 +0700

    up

---
+++ VegetableStore/VegetableStore/Areas/Admin/Controllers/FunctionController.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using VegetableStore.Interfaces;
+using VegetableStore.Models.ViewModels;
+
+namespace VegetableStore.Areas.Admin.Controllers
+{
+    public class FunctionController : BaseController
+    {
+        private IFunctionRepository _functionRepository;
+
+        public FunctionController(IFunctionRepository functionRepository)
+        {
+            this._functionRepository = functionRepository;
+        }
+        [HttpGet]
+        public IActionResult GetAllFillter(string filter)
+        {
+            var model = _functionRepository.GetAll(filter);
+            return new ObjectResult(model);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var model = await _functionRepository.GetAll(string.Empty);
+            var rootFunctions = model.Where(c => c.ParentId == null);
+            var items = new List<FunctionViewModel>();
+            foreach (var function in rootFunctions)
+            {
+                //add the parent category to the item list
+                items.Add(function);
+                //now get all its children (separate Category in case you need recursion)
+                GetByParentId(model.ToList(), function, items);
+            }
+            return new ObjectResult(items);
+        }
+
+        [HttpGet]
+        public IActionResult GetById(string id)
+        {
+            var model = _functionRepository.GetAll(id);
+
+            return new ObjectResult(model);
+        }
+
+        [HttpPost]
+        public IActionResult SaveEntity(FunctionViewModel functionVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                IEnumerable<ModelError> allErrors = ModelState.Values.SelectMany(v => v.Errors);
+                return new BadRequestObjectResult(allErrors);
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(functionVm.Id))
+                {
+                    _functionRepository.Add(functionVm);
+                }
+                else
+                {
+                    _functionRepository.Update(functionVm);
+                }
+                _functionRepository.Save();
+                return new OkObjectResult(functionVm);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult UpdateParentId(string sourceId, string targetId, Dictionary<string, int> items)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+            else
+            {
+                if (sourceId == targetId)
+                {
+                    return new BadRequestResult();
+                }
+                else
+                {
+                    _functionRepository.UpdateParentId(sourceId, targetId, items);
+                    _functionRepository.Save();
+                    return new OkResult();
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult ReOrder(string sourceId, string targetId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestObjectResult(ModelState);
+            }
+            else
+            {
+                if (sourceId == targetId)
+                {
+                    return new BadRequestResult();
+                }
+                else
+                {
+                    _functionRepository.ReOrder(sourceId, targetId);
+                    _functionRepository.Save();
+                    return new OkObjectResult(sourceId);
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return new BadRequestResult();
+            }
+            else
+            {
+                _functionRepository.Delete(id);
+                _functionRepository.Save();
+                return new OkObjectResult(id);
+            }
+        }
+        private void GetByParentId(IEnumerable<FunctionViewModel> allFunctions,
+          FunctionViewModel parent, IList<FunctionViewModel> items)
+        {
+            var functionsEntities = allFunctions as FunctionViewModel[] ?? allFunctions.ToArray();
+            var subFunctions = functionsEntities.Where(c => c.ParentId == parent.Id);
+            foreach (var cat in subFunctions)
+            {
+                //add this category
+                items.Add(cat);
+                //recursive call in case your have a hierarchy more than 1 level deep
+                GetByParentId(functionsEntities, cat, items);
+            }
+        }
+    }
+}
