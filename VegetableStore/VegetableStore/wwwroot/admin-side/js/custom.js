e261aadbeae536341dc553406a15eb0462f1e0da
Phan Thanh Nam <40748333+phannam97@users.noreply.github.com>
2019-08-03 18:56:10 +0700

    up

--- VegetableStore/VegetableStore/wwwroot/admin-side/js/custom.js
+++ VegetableStore/VegetableStore/wwwroot/admin-side/js/custom.js
@@ -292,7 +292,7 @@ if (typeof NProgress != 'undefined') {
         NProgress.start();
     });
 
-    $(window).load(function () {
+    $(window).on('load',function () {
         NProgress.done();
     });
 }
