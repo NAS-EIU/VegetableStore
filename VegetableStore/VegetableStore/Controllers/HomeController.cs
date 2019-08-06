b2a6d30c11751760bff07229d9d0db543ed140fd
Sil <stilling143@gmail.com>
2019-08-06 15:49:00 +0700

    up

--- VegetableStore/VegetableStore/Controllers/HomeController.cs
+++ VegetableStore/VegetableStore/Controllers/HomeController.cs
@@ -4,15 +4,36 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using VegetableStore.Interfaces;
 using VegetableStore.Models;
+using VegetableStore.Models.ViewModels;
 
 namespace VegetableStore.Controllers
 {
     public class HomeController : Controller
     {
+        private IProductRepository _productService;
+        private readonly IStringLocalizer<HomeController> _localizer;
+
+        public HomeController(IProductRepository productService, IStringLocalizer<HomeController> localizer)
+        {
+            _productService = productService;
+            _localizer = localizer;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            
+                //var title = _localizer["Title"];
+                //var culture = HttpContext.Features.Get<IRequestCultureFeature>().RequestCulture.Culture.Name;
+                ViewData["BodyClass"] = "cms-index-index cms-home-page";
+                var homeVm = new HomeViewModel();
+                homeVm.HotProducts = _productService.GetHotProduct(12);
+                homeVm.TopSellProducts = _productService.GetLastest(12);
+
+                return View(homeVm);
+            
         }
 
         public IActionResult About()
